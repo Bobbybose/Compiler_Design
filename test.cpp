cdab42c83192231ee2fa69e9ce5cd7c30c064712
teamdigit24 <bobbybose19@gmail.com>
2023-01-19 15:56:45 -0500

    new

--- test.cpp
+++ test.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    string s;
+
+    cin >> s;
+
+    cout << "Hello new world!" << endl;
 }
